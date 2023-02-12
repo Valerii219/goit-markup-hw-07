f73ee56a8b35b753f6aef11ec4838411bf86f7ba
Valerii219 <valera.arl96@gmail.com>
2023-02-12 16:27:56 +0100

    add menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,6 +3,8 @@
       openMenuBtn: document.querySelector("[data-menu-open]"),
       closeMenuBtn: document.querySelector("[data-menu-close]"),
       menu: document.querySelector("[data-menu]"),
+      body: document.querySelector("body"),
+
     };
   
     refs.openMenuBtn.addEventListener("click", toggleMenu);
@@ -10,5 +12,7 @@
   
     function toggleMenu() {
       refs.menu.classList.toggle("is-hidden");
+      refs.body.classList.toggle("no-scroll");
+
     }
   })();
